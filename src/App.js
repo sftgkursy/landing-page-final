993e6629e017de96a90453032920d7d687cabd9d
centrum skzolen <centrum@wp.pl>
2023-02-02 16:12:04 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
 
-  const firstname = "John";
-  const nums = 2 + 4;
-
   return (
     <div>
-        <h3>To jest {firstname}</h3>
-        <p>Wynik to {nums}</p>
-        <p>{2 + 5 * 6}</p>
+      <h1>Hello</h1>
     </div>
   )
 }
